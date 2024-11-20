779d5bdf22826787fb5e73a60d6cd7694fa7bb86
RahulGhosh <rahul.chandra@bracits.com>
2024-11-20 18:07:33 +0600

    custom instance

--- src/examples/6.interceptors.jsx
+++ src/examples/6.interceptors.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+
 const Interceptors = () => {
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    console.log("Axios Interceptors...");
+  };
+
   return (
     <div className="space-y-10">
       <p className="read-the-docs">
